97c974710267419ff957c8e361dfeb2f9457aed9
Yanhao Jia <yanhaoj@udel.edu>
2022-10-28 15:16:33 -0400

    fixed deploy problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Home from './pages/Home';
 import Dnd from './pages/Dnd';
@@ -8,15 +8,13 @@ function App(): JSX.Element{
   return (
     <>
       <Navbar/>
-      <Container className = "mb-4">
-        {/* <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dnd" element={<Dnd />} />
-        </Routes> */}
-        <Dnd/>
+      <Container>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/dnd' element={<Dnd />}/>
+        </Routes>
       </Container>
     </>
   );
 }
-
 export default App;
